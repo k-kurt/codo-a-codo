8ed1ef64f764749aed8b03497be847178c3e7937
k-kurt <sboboda1997@gmail.com>
2023-04-27 16:52:02 -0600

    clase 15

---
+++ proyecto15/js/clase.js
@@ -0,0 +1,25 @@
+/* var producto="hola mundo";
+console.log(producto); */
+
+let valorDeVerdad;
+
+let numero=5;
+
+/* valorDeVerdad=(5==5); */
+/* valorDeVerdad=!(false);
+ *//* 
+valorDeVerdad=(123>324); */
+
+
+valorDeVerdad=(123>324);
+
+console.log(valorDeVerdad);
+
+
+
+let num1=5, num2=7;
+
+
+
+let max=(num1>num2) ? "el primer numero es el maor"
+: "el segundo es mayor";
