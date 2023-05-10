17168dcb5cbd9b61371fb97266de762ddf4987ca
k-kurt <sboboda1997@gmail.com>
2023-05-09 21:12:27 -0300

    clase 18

---
+++ proyecto18/js/clase.js
@@ -0,0 +1,42 @@
+/* var producto="hola mundo";
+console.log(producto); */
+
+let valorDeVerdad;
+
+let numero=5;
+
+/* valorDeVerdad=(5==5); */
+/* valorDeVerdad=!(false);
+ *//* 
+valorDeVerdad=(123>324); */
+
+
+valorDeVerdad=(123>324);
+
+console.log(valorDeVerdad);
+
+
+
+let num1=5, num2=7;
+
+
+
+let max=(num1>num2) ? "el primer numero es el maor"
+: "el segundo es mayor";
+
+
+let aux= true;
+switch(aux){
+    case true:
+        console.log("hola");
+        brack;
+    default:
+            console.log("askad");
+            brack;
+}
+
+let contador=0;
+while(contador <=20){
+        console.log(contador);
+        contador++;
+}
